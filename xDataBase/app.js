b795640980c2646a04a4e36b55744cf1c506196b
Gabriel <gabrieltroop27@gmail.com>
2024-12-12 15:13:11 -0300

    temque rever a pora toda

--- xDataBase/app.js
+++ xDataBase/app.js
@@ -3,15 +3,13 @@ import { open } from 'sqlite';
 
 
 async function criarEPopularTabelaUsuarios(nome, number, password){
-    const db = await open({
-        filename: './xDataBase/banco.db',
-        driver: sqlite3.Database,
-    });
+  const db = await open({
+      filename: './xDataBase/banco.db',
+      driver: sqlite3.Database,
+  });
 
-    db.run('CREATE TABLE IF NOT EXISTS usuarios (id INTEGER PRIMARY KEY, nome TEXT, number INTEGER, password VARCHAR)');
-    db.run('INSERT INTO usuarios (nome, number, password) VALUES (?,?,?)', [nome, number, password])
-    // db.run('DELETE FROM usuarios WHERE id = 1' )
-    	
+  db.run('CREATE TABLE IF NOT EXISTS usuarios (id INTEGER PRIMARY KEY, nome TEXT, number INTEGER, password VARCHAR)');
+  db.run('INSERT INTO usuarios (nome, number, password) VALUES (?,?,?)', [nome, number, password])
+  // db.run('DELETE FROM usuarios WHERE id = 1' )
+      
 }
-
-criarEPopularTabelaUsuarios('Kaguyo', '999999999');
